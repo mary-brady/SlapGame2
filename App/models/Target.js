5d04348c9f81f5a3f286edf19b78e04a00ed9534
Mary Brady <emceebrady@gmail.com>
2018-08-08 10:29:15 -0600

    project and target set up

---
+++ App/models/Target.js
@@ -0,0 +1,31 @@
+class Target {
+    constructor(name, health, img, item, slapdmg, punchdmg, kickdmg, specialdmg) {
+        this.name = name || ''
+        this.health = health || 100
+        this.img = img || ''
+        this.damages = {
+            slap: slapdmg,
+            punch: punchdmg,
+            kick: kickdmg,
+            special: specialdmg
+        }
+        this.dead = false
+        this.hits = 0
+    }
+
+    attack(type) {
+        if (this.dead) {
+            return
+        }
+        if (this.damages[type]) {
+            this.health -= this.damages[type]
+            this.hits++
+            if (this.health <= 0) {
+                this.health = 0
+                this.dead = true
+            }
+        }
+    }
+}
+
+export default Target
